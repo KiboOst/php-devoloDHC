cef2b2f2ce9bb4f8014ae8ddf36730ba182b28cf
KiboOst <kiboost@free.fr>
2017-03-14 18:56:19 +0100

    v1.23: Code optimization, should run a bit faster!

--- class/phpDevoloAPI.php
+++ class/phpDevoloAPI.php
@@ -2,7 +2,7 @@
 
 class DevoloDHC {
 
-	public $_version = "1.2";
+	public $_version = "1.23";
 
 	function __construct($login, $password, $localHost, $uuid=null, $gateway=null, $passkey=null)
 	{
@@ -78,9 +78,8 @@ class DevoloDHC {
 		$sensors = (isset($device['sensors']) ? json_decode($device['sensors'], true): null);
 		if ($sensors == null) return array('result'=>null, 'error' => 'Unfound sensor for device');
 
-		for($i=0; $i<count($sensors); $i++)
+		foreach ($sensors as $sensor)
 		{
-			$sensor = $sensors[$i];
 			$sensorType = $this->getSensorType($sensor);
 
 			if (in_array($sensorType, $this->_DevicesOnOff))
@@ -111,9 +110,8 @@ class DevoloDHC {
 		//fetch sensors:
 		$arrayStates = array();
 
-		for($i=0; $i<count($sensors); $i++)
+		foreach($sensors as $sensor)
 		{
-			$sensor = $sensors[$i];
 			$sensorType = $this->getSensorType($sensor);
 			$param = $this->getValuesByType($sensorType);
 			if ($param != null)
@@ -149,7 +147,6 @@ class DevoloDHC {
 		if ( isset($device['error']) ) return $device;
 
 		$datas = $this->getDeviceStates($device);
-		$itemCount = count($datas);
 		$availableDatas = array();
 		foreach ($datas['result'] as $item)
 		{
@@ -192,9 +189,9 @@ class DevoloDHC {
 	public function getAllBatteries($lowLevel=100, $filter=1)
 	{
 		$jsonDatas = array();
-		for($i=0; $i<count($this->_AllDevices); $i++)
+		$c = count($this->_AllDevices);
+		foreach ($this->_AllDevices as $thisDevice)
 		{
-			$thisDevice = $this->_AllDevices[$i];
 			$thisDeviceName = $thisDevice['name'] ;
 			$thisBatLevel = $thisDevice['batteryLevel'];
 			if (($thisBatLevel == -1) or ($thisBatLevel == "None")) {if ($filter==1) continue; }
@@ -215,10 +212,9 @@ class DevoloDHC {
 		if (isset($answer['error']["message"]) ) return array('result'=>null, 'error'=>$answer['error']["message"]);
 
 		$jsonDatas = array();
-		$numEvents = count($answer['result']); //may have less than requested
-		for($i=$numEvents-1; $i>=1; $i--) //put recent on top
+		$numEvents = count($answer['result'])-1; //may have less than requested
+		foreach (array_reverse($answer['result']) as $event)
 		{
-			$event = $answer['result'][$i];
 			$deviceName = $event['deviceName'];
 			$deviceZone = $event['deviceZone'];
 			$author = $event['author'];
@@ -249,12 +245,11 @@ class DevoloDHC {
 		if (isset($answer['error']["message"]) ) return array('result'=>null, 'error'=>$answer['error']["message"]);
 
 		$jsonDatas = array();
-		$numItems = count($answer['result']);
-		for($i=0; $i<$numItems; $i++)
+		foreach ($answer['result'] as $item)
 		{
-			$sensor = $answer['result'][$i]['widgetElementUID'];
-			$values = $answer['result'][$i]['value'];
-			$timesOfDay = $answer['result'][$i]['timeOfDay'];
+			$sensor = $item['widgetElementUID'];
+			$values = $item['value'];
+			$timesOfDay = $item['timeOfDay'];
 
 			if (strpos($device['model'], 'Door/Window:Sensor'))
 			{
@@ -274,11 +269,8 @@ class DevoloDHC {
 			}
 
 			$sensorData = array('sensor'=>$sensor);
-
-			for($j=count($values)-1; $j>=1; $j--) //put recent on top
+			foreach (array_reverse($values) as $value => $timeOfDay)
 			{
-				$value = $values[$j];
-				$timeOfDay = $timesOfDay[$j]; //set it readable!
 				$timeOfDay = gmdate("H:i:s", $timeOfDay);
 				$datas = array($timeOfDay=>$value);
 				$sensorData[$timeOfDay] = $value;
@@ -334,7 +326,8 @@ class DevoloDHC {
 			if (!isset($dateEnd)) $dateEnd = $logDateEnd;
 
 			$sumArray = array();
-			for($i=0; $i<count($prevDatas); $i++)
+			$c = count($prevDatas);
+			for($i=0; $i<$c; $i++)
 			{
 				$thisDate = $keys[$i];
 				$data = $prevDatas[$thisDate];
@@ -384,9 +377,8 @@ class DevoloDHC {
 
 		if ($state < 0) $state = 0;
 
-		for($i=0; $i<count($sensors); $i++)
+		foreach ($sensors as $sensor)
 		{
-			$sensor = $sensors[$i];
 			$sensorType = $this->getSensorType($sensor);
 
 			if (in_array($sensorType, $this->_DevicesOnOff))
@@ -429,9 +421,8 @@ class DevoloDHC {
 		$sensors = (isset($device['sensors']) ? json_decode($device['sensors'], true) : null);
 		if ($sensors == null) return array('result'=>null, 'error' => 'No sensor found in this device');
 
-		for($i=0; $i<count($sensors); $i++)
+		foreach ($sensors as $sensor)
 		{
-			$sensor = $sensors[$i];
 			$sensorType = $this->getSensorType($sensor);
 
 			if (in_array($sensorType, $this->_DevicesSendValue))
@@ -455,9 +446,8 @@ class DevoloDHC {
 		$sensors = (isset($device['sensors']) ? json_decode($device['sensors'], true) : null);
 		if ($sensors == null) return array('result'=>null, 'error' => 'No sensor found in this device');
 
-		for($i=0; $i<count($sensors); $i++)
+		foreach($sensors as $sensor)
 		{
-			$sensor = $sensors[$i];
 			$sensorType = $this->getSensorType($sensor);
 			if (in_array($sensorType, $this->_DevicesPressKey))
 			{
@@ -481,9 +471,8 @@ class DevoloDHC {
 	//GET shorcuts:
 	public function getDeviceByName($name)
 	{
-		for($i=0; $i<count($this->_AllDevices); $i++)
+		foreach($this->_AllDevices as $thisDevice)
 		{
-			$thisDevice = $this->_AllDevices[$i];
 			if ($thisDevice['name'] == $name) return $thisDevice;
 		}
 		return array('result'=>null, 'error' => 'Unfound device');
@@ -493,9 +482,8 @@ class DevoloDHC {
 	{
 		if (count($this->_AllRules) == 0) $this->getRules();
 
-		for($i=0; $i<count($this->_AllRules); $i++)
+		foreach($this->_AllRules as $thisRule)
 		{
-			$thisRule = $this->_AllRules[$i];
 			if ($thisRule['name'] == $name) return $thisRule;
 		}
 		return array('result'=>null, 'error' => 'Unfound rule');
@@ -505,9 +493,8 @@ class DevoloDHC {
 	{
 		if (count($this->_AllTimers) == 0) $this->getTimers();
 
-		for($i=0; $i<count($this->_AllTimers); $i++)
+		foreach($this->_AllTimers as $thisTimer)
 		{
-			$thisTimer = $this->_AllTimers[$i];
 			if ($thisTimer['name'] == $name) return $thisTimer;
 		}
 		return array('result'=>null, 'error' => 'Unfound timer');
@@ -517,9 +504,8 @@ class DevoloDHC {
 	{
 		if (count($this->_AllScenes) == 0) $this->getScenes();
 
-		for($i=0; $i<count($this->_AllScenes); $i++)
+		foreach($this->_AllScenes as $thisScene)
 		{
-			$thisScene = $this->_AllScenes[$i];
 			if ($thisScene['name'] == $name) return $thisScene;
 		}
 		return array('result'=>null, 'error' => 'Unfound scene');
@@ -527,9 +513,8 @@ class DevoloDHC {
 
 	public function getGroupByName($name)
 	{
-		for($i=0; $i<count($this->_AllGroups); $i++)
+		foreach($this->_AllGroups as $thisGroup)
 		{
-			$thisGroup = $this->_AllGroups[$i];
 			if ($thisGroup['name'] == $name) return $thisGroup;
 		}
 		return array('result'=>null, 'error' => 'Unfound group');
@@ -542,12 +527,12 @@ class DevoloDHC {
 
 		//get all devices from all zones:
 		$UIDSarray = array();
-		for($i=0; $i<count($this->_AllZones); $i++)
+		foreach ($this->_AllZones as $thisZone)
 		{
-			$thisDevices = $this->_AllZones[$i]["deviceUIDs"];
-			for($j=0; $j<count($thisDevices); $j++)
+			$thisDevices = $thisZone["deviceUIDs"];
+			foreach ($thisDevices as $thisDevice)
 			{
-				array_push($UIDSarray, $thisDevices[$j]);
+				array_push($UIDSarray, $thisDevice);
 			}
 		}
 
@@ -556,11 +541,8 @@ class DevoloDHC {
 
 		//store devices:
 		$devices = array();
-		$devicesNum = count($jsonArray['result']["items"]);
-		for($i=0; $i<$devicesNum; $i++)
+		foreach ($jsonArray['result']["items"] as $thisDevice)
 		{
-			$thisDevice =  $jsonArray['result']["items"][$i];
-
 			$name = (isset($thisDevice["properties"]["itemName"]) ? $thisDevice["properties"]["itemName"] : "None");
 			$uid = (isset($thisDevice["UID"]) ? $thisDevice["UID"] : "None");
 			$elementUIDs = (isset($thisDevice["properties"]["elementUIDs"]) ? $thisDevice["properties"]["elementUIDs"] : "None");
@@ -588,20 +570,18 @@ class DevoloDHC {
 				["devolo.Grouping"],0
 				]
 			}';
-		$json = json_decode($json);
-		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 
+		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 
 		//get all zones:
 
 		$zones = $jsonArray['result']["items"][0]['properties']['zones'];
-		$zonesNum = count($zones);
-		for($i=0; $i<$zonesNum; $i++)
+		foreach ($zones as $thisZone)
 		{
-			$thisID = $zones[$i]["id"];
-			$thisName = $zones[$i]["name"];
-			$thisDevices = $zones[$i]["deviceUIDs"];
+			$thisID = $thisZone["id"];
+			$thisName = $thisZone["name"];
+			$thisDevices = $thisZone["deviceUIDs"];
 
 			$zone = array("name" => $thisName,
 							"id" => $thisID,
@@ -610,16 +590,11 @@ class DevoloDHC {
 			array_push($this->_AllZones, $zone);
 		}
 
-		//get all groups:
-		$groups = $jsonArray['result']["items"][0]['properties']['smartGroupWidgetUIDs'];
-		$groupsNum = count($groups);
 		//get each group infos:
-		$jsonArray = $this->fetchItems($groups);
+		$jsonArray = $this->fetchItems($jsonArray['result']["items"][0]['properties']['smartGroupWidgetUIDs']);
 
-		$groups = $jsonArray['result']["items"];
-		for($i=0; $i<$groupsNum; $i++)
+		foreach ($jsonArray['result']["items"] as $thisGroup)
 		{
-			$thisGroup = $groups[$i];
 			$thisID = $thisGroup["UID"];
 			$thisName = $thisGroup['properties']["itemName"];
 			$thisOurOfSync = $thisGroup['properties']["outOfSync"];
@@ -647,30 +622,20 @@ class DevoloDHC {
 				["devolo.Scene"],0
 				]
 			}';
-		$json = json_decode($json);
+
 		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 
-		$scenesArray = array();
-		$scenesNum = count($jsonArray['result']["items"][0]['properties']['sceneUIDs']);
-		for($i=0; $i<$scenesNum; $i++)
-		{
-			$thisScene = $jsonArray['result']["items"][0]['properties']['sceneUIDs'][$i];
-			array_push($scenesArray, $thisScene);
-		}
+		//request datas for all scenes:
+		$jsonArray = $this->fetchItems($jsonArray['result']["items"][0]['properties']['sceneUIDs']);
 
-		//request datas for all rules:
-		$jsonArray = $this->fetchItems($scenesArray);
-
-		$scenesNum = count($jsonArray['result']["items"]);
-		for($i=0; $i<$scenesNum; $i++)
+		foreach($jsonArray['result']["items"] as $thisScene)
 		{
-			$thisScene = $jsonArray['result']["items"][$i];
-			$rule = array("name" => $thisScene["properties"]["itemName"],
+			$scene = array("name" => $thisScene["properties"]["itemName"],
 							"id" => $thisScene["UID"],
 							"element" => str_replace("Scene", "SceneControl", $thisScene["UID"])
 							);
-			array_push($this->_AllScenes, $rule);
+			array_push($this->_AllScenes, $scene);
 		}
 	}
 
@@ -685,25 +650,15 @@ class DevoloDHC {
 				["devolo.Schedules"],0
 				]
 			}';
-		$json = json_decode($json);
+
 		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 
-		$timersArray = array();
-		$timersNum = count($jsonArray['result']["items"][0]['properties']['scheduleUIDs']);
-		for($i=0; $i<$timersNum; $i++)
-		{
-			$thisTimer = $jsonArray['result']["items"][0]['properties']['scheduleUIDs'][$i];
-			array_push($timersArray, $thisTimer);
-		}
-
 		//request datas for all rules:
-		$jsonArray = $this->fetchItems($timersArray);
+		$jsonArray = $this->fetchItems($jsonArray['result']["items"][0]['properties']['scheduleUIDs']);
 
-		$timersNum = count($jsonArray['result']["items"]);
-		for($i=0; $i<$timersNum; $i++)
+		foreach($jsonArray['result']["items"] as $thisTimer)
 		{
-			$thisTimer = $jsonArray['result']["items"][$i];
 			$rule = array("name" => $thisTimer["properties"]["itemName"],
 							"id" => $thisTimer["UID"],
 							"element" => str_replace("Schedule", "ScheduleControl", $thisTimer["UID"])
@@ -723,25 +678,15 @@ class DevoloDHC {
 				["devolo.Services"],0
 				]
 			}';
-		$json = json_decode($json);
+
 		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 
-		$rulesArray = array();
-		$rulesNum = count($jsonArray['result']["items"][0]['properties']['serviceUIDs']);
-		for($i=0; $i<$rulesNum; $i++)
-		{
-			$thisRule = $jsonArray['result']["items"][0]['properties']['serviceUIDs'][$i];
-			array_push($rulesArray, $thisRule);
-		}
-
 		//request datas for all rules:
-		$jsonArray = $this->fetchItems($rulesArray);
+		$jsonArray = $this->fetchItems($jsonArray['result']["items"][0]['properties']['serviceUIDs']);
 
-		$rulesNum = count($jsonArray['result']["items"]);
-		for($i=0; $i<$rulesNum; $i++)
+		foreach($jsonArray['result']["items"] as $thisRule)
 		{
-			$thisRule = $jsonArray['result']["items"][$i];
 			$rule = array("name" => $thisRule["properties"]["itemName"],
 							"id" => $thisRule["UID"],
 							"element" => str_replace("Service", "ServiceControl", $thisRule["UID"])
@@ -812,65 +757,70 @@ class DevoloDHC {
 
 
 	//calling functions===================================================
-	protected function _request($protocol, $method, $host, $path, $json, $login, $password, $cookie) //standard function handling all get/post request with curl | return string
+	protected function _request($protocol, $method, $host, $path, $jsonString, $login, $password, $cookie) //standard function handling all get/post request with curl | return string
 	{
-		$url = $protocol."://".$host.$path;
-
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($curl, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0');
-		curl_setopt($curl, CURLOPT_AUTOREFERER, true);
-
-		if ($protocol == 'https')
+		if (!isset($this->_curlHdl))
 		{
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			$this->_curlHdl = curl_init();
+			curl_setopt($this->_curlHdl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($this->_curlHdl, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($this->_curlHdl, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0');
+			curl_setopt($this->_curlHdl, CURLOPT_AUTOREFERER, true);
+			curl_setopt($this->_curlHdl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($this->_curlHdl, CURLOPT_HTTPHEADER, array(
+														'Connection: keep-alive',
+														'Content-Type: application/json'
+														)
+													);
+			curl_setopt($this->_curlHdl,CURLOPT_ENCODING , '');
 		}
-		else
+
+		$url = $protocol."://".$host.$path;
+		curl_setopt($this->_curlHdl, CURLOPT_URL, $url);
+
+		if ($protocol == 'http')
 		{
-			curl_setopt($curl, CURLOPT_HEADER, true);
-			curl_setopt($curl, CURLINFO_HEADER_OUT, true );
+			curl_setopt($this->_curlHdl, CURLOPT_HEADER, true);
+			curl_setopt($this->_curlHdl, CURLINFO_HEADER_OUT, true );
 		}
 
 		if ( isset($login) and isset($password) )
 		{
 			$auth = $login.":".$password;
-			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_setopt($curl, CURLOPT_USERPWD, $auth);
+			curl_setopt($this->_curlHdl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($this->_curlHdl, CURLOPT_USERPWD, $auth);
 		}
 
 		if ($method == 'POST')
 			{
-				$json->{'id'}=$this->_POSTid;
+				$jsonString = str_replace('"jsonrpc":"2.0",', '"jsonrpc":"2.0", "id":'.$this->_POSTid.',', $jsonString);
 				$this->_POSTid++;
-				$data_string = json_encode($json);
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-														'Content-Type: application/json',
-														'Content-Length: '.strlen($data_string))
-													);
-				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				curl_setopt($this->_curlHdl, CURLOPT_CUSTOMREQUEST, 'POST');
+				curl_setopt($this->_curlHdl, CURLOPT_POSTFIELDS, $jsonString);
 			}
 
 		if ( isset($cookie) ) //auth ok and get connection cookie, can ask and send stuff to central !!
 		{
 			$addCookies = 'JSESSIONID='.$cookie.'; GW_ID='.$this->_gateway.'; FIM_WS_FILTER=(|(GW_ID='.$this->_gateway.')(!(GW_ID=*)))';
-			//$addCookies = 'JSESSIONID='.$cookie;
-			curl_setopt($curl, CURLOPT_COOKIE, $addCookies);
+			curl_setopt($this->_curlHdl, CURLOPT_COOKIE, $addCookies);
 
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLINFO_HEADER_OUT, false );
+			curl_setopt($this->_curlHdl, CURLOPT_HEADER, false);
+			curl_setopt($this->_curlHdl, CURLINFO_HEADER_OUT, false );
 		}
 
-		$response = curl_exec($curl);
-		$header   = curl_getinfo($curl);
-		$err      = curl_errno($curl);
-		$errmsg   = curl_error($curl);
+		$response = curl_exec($this->_curlHdl);
 
-		curl_close($curl);
+		//$info   = curl_getinfo($this->_curlHdl);
+		//echo "<pre>cURL info".json_encode($info, JSON_PRETTY_PRINT)."</pre><br>";
 
-		return $response;
+		if($response === false)
+		{
+			echo 'cURL error: ' . curl_error($this->_curlHdl);
+		}
+		else
+		{
+			return $response;
+		}
 	}
 
 	protected function fetchItems($UIDSarray) //get infos from central for array of device, sensor, timer etc | return array
@@ -884,7 +834,6 @@ class DevoloDHC {
 				]
 			}';
 
-		$json = json_decode($json);
 		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 		return $jsonArray;
@@ -898,16 +847,14 @@ class DevoloDHC {
 			"method":"FIM/invokeOperation",
 			"params":["'.$sensor.'","'.$operation.'",'.$value.']}';
 
-		$json = json_decode($jsonString);
-		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
+		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $jsonString, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 		return $jsonArray;
 	}
 
 	public function sendCommand($jsonString) //directly send json to central. Only works when all required authorisations are set | return array
 	{
-		$json = json_decode($jsonString);
-		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $json, null, null, $this->_sessionID);
+		$data = $this->_request('http', 'POST', $this->_localHost, '/remote/json-rpc', $jsonString, null, null, $this->_sessionID);
 		$jsonArray = json_decode($data, true);
 		return $jsonArray;
 	}
@@ -968,6 +915,7 @@ class DevoloDHC {
 	protected $_Host = 'www.mydevolo.com';
 	protected $_apiVersion = '/v1';
 	protected $_POSTid = 0;
+	protected $_curlHdl = null;
 
 	//user central stuff:
 	protected $_login;
